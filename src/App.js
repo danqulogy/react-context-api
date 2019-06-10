dde92f90a7ae8fbeb1c7072afd16d18991b8dc3f
Bernard D. White <danquahwhite@gmail.com>
2019-06-10 12:05:06 +0000

    Demonstrated how context context was managed before the React Context API

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Parent({name}){
+  return (
+      <div>
+        <h1>Parent</h1>
+        <Child name={name}/>
+      </div>
+  )
+}
+
+function Child({name}) {
+  return (
+      <div>
+        <h1>Child</h1>
+        <GrandChild name={name}/>
+      </div>
+  )
+}
+
+function GrandChild({name}) {
+  return (
+      <div>
+        <h1>Grand Child</h1>
+        <h3>Name: {name}</h3>
+      </div>
+  )
+}
 function App() {
+  const name = "Bernard";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parent name={name}/>
     </div>
   );
 }
